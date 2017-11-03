e083a2287d54b1f523e48b851515fba4386c5460
ojus93 <sangoiojus@gmail.com>
2017-11-03 14:19:56 +0530

    removed delete method from the validity check
    removed delete method from the validity check, so that the fields/body gets the data passed in request params

--- utils.js
+++ utils.js
@@ -98,7 +98,7 @@ utils.defaultTypes = function defaultTypes (types) {
  * @api private
  */
 utils.isValid = function isValid (method) {
-  return ['GET', 'HEAD', 'DELETE'].indexOf(method.toUpperCase()) === -1
+  return ['GET', 'HEAD'].indexOf(method.toUpperCase()) === -1
 }
 
 /**
